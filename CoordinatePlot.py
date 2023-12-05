172f7ae11dae3e07a7eafe310310e1d6d95adfae
tbelvin2000 <46515299+tbelvin2000@users.noreply.github.com>
2023-12-04 19:24:30 -0500

    need to label angles and particle track

--- CoordinatePlot.py
+++ CoordinatePlot.py
@@ -7,8 +7,8 @@ ax = plt.figure().add_subplot(projection='3d')
 t = np.linspace (-10, 10, 10000) 
 v = 1
 b = 4
-theta = np.pi / 4   # 45 degrees
-phi = np.pi / 3     # 90 degrees    
+theta = np.pi / 6   # 45 degrees
+phi = np.pi / 3     # 90 degrees  
 z = v * t * np.cos(theta)
 x = b * np.cos(phi) + v * t * np.sin(theta) * np.sin(phi)
 y = b * np.sin(phi) - v * t * np.sin(theta) * np.cos(phi)
@@ -16,15 +16,23 @@ y = b * np.sin(phi) - v * t * np.sin(theta) * np.cos(phi)
 # Plot axes
 ax.plot(np.linspace(0, 8, 100), np.zeros(100), np.zeros(100), color="grey")
 ax.plot(np.zeros(100), np.linspace(0, 6, 100), np.zeros(100), color="grey")
-ax.plot(np.zeros(t.shape), np.zeros(t.shape), t, label='wire (z-axes)')
+ax.plot(np.zeros(t.shape), np.zeros(t.shape), t, color = "silver")
 # Plot b
 ax.plot(np.linspace(0, b * np.cos(phi), 100), np.linspace(0, b * np.sin(phi), 100), np.zeros(100))
+# Plot phi and theta
+angle = np.linspace(0, 1, 100)
+ax.plot(b/6 * np.cos(angle * phi), b/6 * np.sin(angle * phi), 0)
+ax.plot(b * np.cos(phi) + 2 * np.sin(angle * theta) * np.sin(phi), b * np.sin(phi) - 2 * np.sin(angle * theta) * np.cos(phi), 2 * np.cos(angle * theta))
+ax.plot(b * np.cos(phi) * np.ones(100), b * np.sin(phi) * np.ones(100), np.linspace(-10, 10,100), linestyle='--', color="gray", alpha=0.5)
 # Plot particle track
 ax.plot(x, y, z, label='Particle Track')
 ax.legend()
 
 ax.text(8, 0, -1.5, "x axes")
 ax.text(0, 6, -1.5, "y axes")
+ax.text(0, .1, 10, "wire (z axes)")
 ax.text(b * np.cos(phi) / 2, b * np.sin(phi) / 2, -2, "b")
+
+ax.axis("off")
 
 plt.show()
